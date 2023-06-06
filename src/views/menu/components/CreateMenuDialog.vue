253f74cc7572b53cb52c96c062148ff5d9851228
陈飞龙 <1696741038@qq.com>
2023-06-06 21:53:58 +0800

    样式

--- src/views/menu/components/CreateMenuDialog.vue
+++ src/views/menu/components/CreateMenuDialog.vue
@@ -1,98 +1,131 @@
 <!--新增菜单的弹框-->
 <template>
-  <el-dialog title="新增菜单" width="700px" :visible.sync="visible" @close="close">
-    <el-form ref="addMenuForm" :model="menu" :rules="rules" label-width="100px" :inline="true" label-position="right">
-      <el-form-item label="上级菜单:" prop="parentId">
-        <!--  选择器选项以树形控件展示  -->
-        <el-select ref="selectParentMenu" v-model="parentMenu.id" placeholder="请选择上级菜单">
-          <el-option :key="parentMenu.id" :value="parentMenu.id" :label="parentMenu.name" hidden />
-          <el-tree
-            :data="menuData"
-            :props="menuProps"
-            node-key="id"
-            accordion
-            highlight-current
-            @node-click="handleMenuNodeClick"
-          />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="菜单类型:" prop="type">
-        <el-radio-group v-model="menu.type">
-          <el-radio :label="0">接口</el-radio>
-          <el-radio :label="1">菜单</el-radio>
-          <el-radio :label="2">按钮</el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="菜单名称:" prop="name">
-        <el-input v-model="menu.name" clearable placeholder="请输入菜单名称" />
-      </el-form-item>
-      <el-form-item label="打开方式:" prop="openModel">
-        <el-radio-group v-model="menu.openModel">
-          <el-radio :label="0">内链</el-radio>
-          <el-radio :label="1">外链</el-radio>
-        </el-radio-group>
-      </el-form-item>
+  <el-dialog title="新增菜单" width="720px" :visible.sync="visible" @close="close">
+    <el-form ref="addMenuForm1" :model="menu" :rules="rules" label-width="100px" :inline="true" label-position="right">
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="上级菜单:" prop="parentId">
+            <!--  选择器选项以树形控件展示  -->
+            <el-select ref="selectParentMenu" v-model="parentMenu.id" placeholder="请选择上级菜单">
+              <el-option :key="parentMenu.id" :value="parentMenu.id" :label="parentMenu.name" hidden />
+              <el-tree
+                :data="menuData"
+                :props="menuProps"
+                node-key="id"
+                accordion
+                highlight-current
+                @node-click="handleMenuNodeClick"
+              />
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="菜单类型:" prop="type">
+            <el-radio-group v-model="menu.type">
+              <el-radio :label="0">接口</el-radio>
+              <el-radio :label="1">菜单</el-radio>
+              <el-radio :label="2">按钮</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="菜单名称:" prop="name">
+            <el-input v-model="menu.name" clearable placeholder="请输入菜单名称" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="打开方式:" prop="openModel">
+            <el-radio-group v-model="menu.openModel">
+              <el-radio :label="0">内链</el-radio>
+              <el-radio :label="1">外链</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+      </el-row>
       <!--分隔符-->
-      <div class="cutting-line el-divider el-divider--horizontal" />
+      <el-row>
+        <el-col :span="24">
+          <div class="cutting-line el-divider el-divider--horizontal" />
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="菜单图标:" prop="remark">
+            <el-select v-model="menu.icon" placeholder="请选择菜单图标" class="icon-input" prefix-icon="el-icon-search" :class="iconInputClass" clearable>
 
-      <el-form-item label="菜单图标:" prop="remark">
-        <el-select v-model="menu.icon" placeholder="请选择菜单图标" class="icon-input" clearable>
-          <template #prefix>
-            <span style="padding-left: 5px;">
-              <i :class="menu.icon" />
-            </span>
-          </template>
-          <el-option
-            v-for="item in icons"
-            :key="item"
-            :label="item"
-            :value="item"
-          >
-            <span><i :class="item" /> {{ item }}</span>
-          </el-option>
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="权限标识:" prop="permissionId">
-        <el-input v-model="menu.permissionId" placeholder="请输入权限标识" clearable />
-      </el-form-item>
-
-      <el-form-item label="路由地址:" prop="path">
-        <el-input v-model="menu.path" placeholder="请输入路由地址" clearable />
-      </el-form-item>
-      <el-form-item label="请求方式:" prop="method">
-        <el-select
-          v-model="menu.method"
-          multiple
-          collapse-tags
-          placeholder="请选择请求方式"
-        >
-          <el-option
-            v-for="item in methods"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          />
-        </el-select>
-      </el-form-item>
-
-      <el-form-item label="排序号:" prop="sortNum">
-        <el-input-number v-model="menu.sortNum" controls-position="right" :min="1" :max="99999" />
-      </el-form-item>
-      <el-form-item label="是否展示:" prop="hide">
-        <el-switch
-          v-model="menu.hide"
-          active-color="#13ce66"
-          inactive-color="#ff4949"
-        />
-      </el-form-item>
+              <el-option
+                v-for="item in icons"
+                :key="item"
+                :label="item"
+                :value="item"
+              >
+                <span><i :class="item" /> {{ item }}</span>
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="权限标识:" prop="permissionId">
+            <el-input v-model="menu.permissionId" placeholder="请输入权限标识" clearable />
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="路由地址:" prop="path">
+            <el-input v-model="menu.path" placeholder="请输入路由地址" clearable />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="请求方式:" prop="method">
+            <el-select
+              v-model="menu.method"
+              multiple
+              collapse-tags
+              placeholder="请选择请求方式"
+            >
+              <el-option
+                v-for="item in methods"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="12">
+          <el-form-item label="排序号:" prop="sortNum">
+            <el-input-number v-model="menu.sortNum" controls-position="right" :min="1" :max="99999" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="12">
+          <el-form-item label="是否展示:" prop="hide">
+            <el-switch
+              v-model="menu.hide"
+              :active-value="false"
+              :inactive-value="true"
+            />
+            <el-tooltip placement="top">
+              <div slot="content">选择不展示只注册路由不显示在侧边栏，比如添加页面应该选择不展示</div>
+              <i class="el-icon-warning-outline question" />
+            </el-tooltip>
+          </el-form-item>
+        </el-col>
+      </el-row>
     </el-form>
-    <el-form :model="menu" :rules="rules" label-width="100px" label-position="right">
+    <el-form ref="addMenuForm2" :model="menu" :rules="rules" label-width="100px" label-position="right">
       <el-form-item label="菜单元数据:" prop="metadata">
-        <el-input v-model="menu.metadata" type="textarea" :rows="5" />
+        <el-input v-model="menu.metadata" type="textarea" :rows="5" placeholder="请输入JSON格式的路由元数据" />
       </el-form-item>
     </el-form>
-    <div slot="footer" class="dialog-footer" />
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="resetForm()">取 消</el-button>
+      <el-button type="primary" @click="submitForm()">保 存</el-button>
+    </div>
   </el-dialog>
 </template>
 
@@ -100,7 +133,6 @@
 
 import { EL_ICONS } from "@/constant/commons";
 import { isJSON } from "@/utils/validate";
-
 export default {
   name: 'CreateMenuDialog',
   props: {
@@ -137,6 +169,7 @@ export default {
         name: ''
       },
       icons: [],
+      iconInputClass: 'default-icon-input-Class',
       methods: [
         { value: 'GET', label: 'GET' },
         { value: 'POST', label: 'POST' },
@@ -164,16 +197,16 @@ export default {
       },
       rules: {
         name: [
-          { required: true, message: '请输入菜单名称', triangle: 'blur' }
+          { required: true, message: '请输入菜单名称', trigger: 'blur' }
         ],
         remark: [
-          { required: false, max: 255, message: '备注限制最多255字符', triangle: 'blur' }
+          { required: false, max: 255, message: '备注限制最多255字符', trigger: 'blur' }
         ],
         sortNum: [
-          { required: true, type: 'number', min: 1, max: 99999, message: '请输入排序号', triangle: 'blur' }
+          { required: true, type: 'number', min: 1, max: 99999, message: '请输入排序号', trigger: 'blur' }
         ],
         metadata: [
-          { required: false, message: '请输入正确的JSON格式', triangle: 'blur', validator: validateMetadata }
+          { required: false, message: '请输入正确的JSON格式', trigger: 'blur', validator: validateMetadata }
         ]
       }
     };
@@ -189,6 +222,14 @@ export default {
         }
       }
     },
+    'menu.icon'() {
+      console.log(11)
+      if (this.menu.icon === '') {
+        this.iconInputClass = 'default-icon-input-class'
+      } else {
+        this.iconInputClass = 'icon-input-class';
+      }
+    }
   },
   methods: {
     handleMenuNodeClick(data) {
@@ -201,7 +242,29 @@ export default {
       // 使 input 失去焦点，并隐藏下拉框
       this.$refs.selectParentMenu.blur()
     },
-    submitForm(formName) {
+    // 重置表单
+    resetForm() {
+      this.parentMenu = {
+        id: '',
+        name: ''
+      }
+      this.menu = {
+        parentId: undefined,
+        type: 0,
+        name: undefined,
+        openModel: 0,
+        icon: '',
+        permissionId: undefined,
+        path: undefined,
+        method: undefined,
+        sortNum: 0,
+        hide: false,
+        metadata: undefined,
+        remark: '',
+      }
+      this.close();
+    },
+    submitForm() {
       // this.$refs[formName].validate((valid) => {
       //   if (valid) {
       //     // addRole(this.role).then(response => {
@@ -227,9 +290,37 @@ export default {
   margin: 8px 0 30px 0;
 }
 .el-input--medium .el-input__inner{
-  width: 270px;
+  //width: 300px !important;
 }
 .icon-input{
   width: 202px;
 }
+.question{
+  display: inline-block;
+  position: absolute;
+  margin: 13px 6px;
+}
+.el-form-item {
+  .el-input-number--medium {
+    width: 230px;
+  }
+  .el-input {
+    width: 230px;
+  }
+  .el-select{
+    width: 230px;
+  }
+}
+.default-icon-input-class{
+  .el-input--prefix .el-input__inner{
+    padding-left: 15px !important;
+  }
+}
+.icon-input-class{
+  input {
+    padding-left: 30px;
+  }
+
+}
+
 </style>
