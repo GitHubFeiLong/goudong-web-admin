50dfdc590d8f4db05c53b06d4fcc2aac788fc457
cfl <1696741038@qq.com>
2023-06-06 18:29:16 +0800

    菜单

--- src/views/menu/components/CreateMenuDialog.vue
+++ src/views/menu/components/CreateMenuDialog.vue
@@ -16,44 +16,80 @@
           />
         </el-select>
       </el-form-item>
-      <el-form-item label="菜单类型:" prop="remark">
-        <el-radio-group v-model="menu.remark">
+      <el-form-item label="菜单类型:" prop="type">
+        <el-radio-group v-model="menu.type">
           <el-radio :label="0">接口</el-radio>
           <el-radio :label="1">菜单</el-radio>
           <el-radio :label="2">按钮</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="菜单名称:" prop="remark">
-        <el-input v-model="menu.remark" clearable placeholder="请输入菜单名称" />
+      <el-form-item label="菜单名称:" prop="name">
+        <el-input v-model="menu.name" clearable placeholder="请输入菜单名称" />
       </el-form-item>
+      <el-form-item label="打开方式:" prop="openModel">
+        <el-radio-group v-model="menu.openModel">
+          <el-radio :label="0">内链</el-radio>
+          <el-radio :label="1">外链</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <!--分隔符-->
+      <div class="cutting-line el-divider el-divider--horizontal" />
+
       <el-form-item label="菜单图标:" prop="remark">
-        <el-select v-model="menu.icon" placeholder="请选择活动区域">
+        <el-select v-model="menu.icon" placeholder="请选择菜单图标" class="icon-input" clearable>
+          <template #prefix>
+            <span style="padding-left: 5px;">
+              <i :class="menu.icon" />
+            </span>
+          </template>
           <el-option
             v-for="item in icons"
             :key="item"
             :label="item"
             :value="item"
+          >
+            <span><i :class="item" /> {{ item }}</span>
+          </el-option>
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="权限标识:" prop="permissionId">
+        <el-input v-model="menu.permissionId" placeholder="请输入权限标识" clearable />
+      </el-form-item>
+
+      <el-form-item label="路由地址:" prop="path">
+        <el-input v-model="menu.path" placeholder="请输入路由地址" clearable />
+      </el-form-item>
+      <el-form-item label="请求方式:" prop="method">
+        <el-select
+          v-model="menu.method"
+          multiple
+          collapse-tags
+          placeholder="请选择请求方式"
+        >
+          <el-option
+            v-for="item in methods"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
           />
         </el-select>
       </el-form-item>
-      <!--分隔符-->
-      <div class="cutting-line el-divider el-divider--horizontal" />
-      <el-form-item label="路由地址" prop="remark">
-        <el-input v-model="menu.remark" placeholder="请输入路由地址" clearable />
-      </el-form-item>
-      <el-form-item label="请求方式" prop="remark">
-        <el-input v-model="menu.remark" clearable />
-      </el-form-item>
 
-      <el-form-item label="排序号" prop="remark">
-        <el-input v-model="menu.remark" clearable />
+      <el-form-item label="排序号:" prop="sortNum">
+        <el-input-number v-model="menu.sortNum" controls-position="right" :min="1" :max="99999" />
       </el-form-item>
-      <el-form-item label="是否展示" prop="remark">
-        <el-input v-model="menu.remark" clearable />
+      <el-form-item label="是否展示:" prop="hide">
+        <el-switch
+          v-model="menu.hide"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        />
       </el-form-item>
-
-      <el-form-item label="菜单元数据" prop="remark">
-        <el-input v-model="menu.remark" clearable />
+    </el-form>
+    <el-form :model="menu" :rules="rules" label-width="100px" label-position="right">
+      <el-form-item label="菜单元数据:" prop="metadata">
+        <el-input v-model="menu.metadata" type="textarea" :rows="5" />
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer" />
@@ -63,6 +99,7 @@
 <script>
 
 import { EL_ICONS } from "@/constant/commons";
+import { isJSON } from "@/utils/validate";
 
 export default {
   name: 'CreateMenuDialog',
@@ -77,6 +114,18 @@ export default {
     }
   },
   data() {
+    const validateMetadata = (rule, value, callback) => {
+      isJSON(value).then(boo => {
+        console.log(boo)
+        if (!boo) {
+          console.log(1)
+          callback(new Error(""));
+        } else {
+          console.log(2)
+          callback();
+        }
+      })
+    };
     return {
       menuData: undefined,
       menuProps: {
@@ -88,15 +137,43 @@ export default {
         name: ''
       },
       icons: [],
+      methods: [
+        { value: 'GET', label: 'GET' },
+        { value: 'POST', label: 'POST' },
+        { value: 'PUT', label: 'PUT' },
+        { value: 'DELETE', label: 'DELETE' },
+        { value: 'HEAD', label: 'HEAD' },
+        { value: 'PATCH', label: 'PATCH' },
+        { value: 'OPTIONS', label: 'OPTIONS' },
+        { value: 'TRACE', label: 'TRACE' },
+      ],
       visible: false,
       menu: {
         parentId: undefined,
+        type: 0,
+        name: undefined,
+        openModel: 0,
         icon: '',
+        permissionId: undefined,
+        path: undefined,
+        method: undefined,
+        sortNum: 0,
+        hide: false,
+        metadata: undefined,
         remark: '',
       },
       rules: {
+        name: [
+          { required: true, message: '请输入菜单名称', triangle: 'blur' }
+        ],
         remark: [
           { required: false, max: 255, message: '备注限制最多255字符', triangle: 'blur' }
+        ],
+        sortNum: [
+          { required: true, type: 'number', min: 1, max: 99999, message: '请输入排序号', triangle: 'blur' }
+        ],
+        metadata: [
+          { required: false, message: '请输入正确的JSON格式', triangle: 'blur', validator: validateMetadata }
         ]
       }
     };
@@ -148,5 +225,11 @@ export default {
 
 .cutting-line{
   margin: 8px 0 30px 0;
+}
+.el-input--medium .el-input__inner{
+  width: 270px;
+}
+.icon-input{
+  width: 202px;
 }
 </style>
